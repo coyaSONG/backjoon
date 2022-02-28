31e2a09b36fcf00c286070ea3e11c984716023e4
coyaSONG <66289470+coyaSONG@users.noreply.github.com>
2022-02-28 20:22:26 +0900

    Create 2562.cs

---
+++ 1차원 배열/2562.cs
@@ -0,0 +1,19 @@
+using System;
+
+namespace backjoon
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int cnt = 0;
+            int max = 0;
+            for(int i=0;i<9;i++)
+            {
+                int n = int.Parse(Console.ReadLine());
+                if(max <= n) { max = n; cnt = i+1; }
+            }
+            Console.WriteLine(max+"\n"+cnt);
+        }
+    }
+}
