629725fee0ff0e4c7eec41443918539dee7a4a26
coyaSONG <66289470+coyaSONG@users.noreply.github.com>
2022-02-28 20:18:31 +0900

    Create 1110.cs

---
+++ while문/1110.cs
@@ -0,0 +1,24 @@
+using System;
+
+namespace backjoon
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int inputNum = int.Parse(Console.ReadLine());
+            int cnt=0;
+            int num=inputNum;
+            
+            while(true)
+            {
+                num = (num%10)*10 + ((num/10)+(num%10))%10;
+                
+                cnt++;
+                
+                if(num == inputNum) break;
+            }
+            Console.WriteLine(cnt);
+        }
+    }
+}
