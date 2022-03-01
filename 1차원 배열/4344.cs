646c65334c80da5fe16ba0682f563e3ccf502ac0
coyaSONG <66289470+coyaSONG@users.noreply.github.com>
2022-03-02 00:55:54 +0900

    Create 4344.cs

---
+++ 1차원 배열/4344.cs
@@ -0,0 +1,40 @@
+using System;
+
+namespace backjoon
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] str = Console.ReadLine().Split();
+
+                float p = float.Parse(str[0]);
+                float cnt = 0;
+                float sum = 0;
+
+                for (int j = 1; j <= p; j++)
+                {
+                    sum += float.Parse(str[j]);
+                }
+
+                float avg = 0;
+                avg = sum / p;
+
+                for (int k = 1; k <= p; k++)
+                {
+                    if (avg < float.Parse(str[k])) cnt++;
+                }
+
+                string fmt = string.Format("{0:f3}", Math.Round(cnt / p * 100, 3));
+
+                Console.WriteLine(fmt+"%");
+            }
+            
+        }
+    }
+}
