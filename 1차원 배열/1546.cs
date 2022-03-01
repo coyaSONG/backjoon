e4252f436be5bda53463c998a25d6bf98cc72eaf
coyaSONG <66289470+coyaSONG@users.noreply.github.com>
2022-03-01 23:32:17 +0900

    Create 1546.cs

---
+++ 1차원 배열/1546.cs
@@ -0,0 +1,31 @@
+using System;
+
+namespace backjoon
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            string[] str = Console.ReadLine().Split();
+            float[] numList = new float[n];
+            float max = int.Parse(str[0]);
+            float rst = 0;
+
+            for(int i=0;i<n;i++)
+            {
+                numList[i] = int.Parse(str[i]);
+                if (max < numList[i]) max = numList[i];
+            }
+
+            for (int i = 0; i < n; i++)
+            {
+                numList[i] = (numList[i] / max) * 100;
+                rst += numList[i];
+            }
+
+            Console.WriteLine(rst / n);
+
+        }
+    }
+}
