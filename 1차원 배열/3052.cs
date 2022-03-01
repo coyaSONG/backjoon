c5f9ec8f34bc050b78d08ecdfa24f96e090ecd2a
coyaSONG <66289470+coyaSONG@users.noreply.github.com>
2022-03-01 23:04:59 +0900

    Create 3052.cs

---
+++ 1차원 배열/3052.cs
@@ -0,0 +1,29 @@
+using System;
+
+namespace backjoon
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numList = new int[10];
+            for (int i=0;i<10;i++)
+            {
+                numList[i] = int.Parse(Console.ReadLine()) % 42;
+            }
+
+            int ans = 0;
+            for(int i=0; i<10;i++)
+            {
+                int cnt = 0;
+                for (int j = i+1; j < 10; j++)
+                {
+                    if (numList[i] == numList[j]) cnt++;
+                }
+                if (cnt == 0) ans++;
+            }
+            Console.WriteLine(ans);
+
+        }
+    }
+}
