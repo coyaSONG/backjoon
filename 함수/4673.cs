ab911cdf0fc12890a57f917dd7408a81974d4dbc
coyaSONG <66289470+coyaSONG@users.noreply.github.com>
2022-03-02 21:33:14 +0900

    Create 4673.cs

---
+++ 함수/4673.cs
@@ -0,0 +1,37 @@
+using System;
+
+namespace baekjoon
+{
+    class p4673
+    {
+        static void Main(string[] args)
+        {
+            int[] numList = new int[10001];
+            for(int i=1;i<=10000;i++)
+            {
+                int n = dn(i);
+                if(n > 10000) continue;
+                numList[n] = 1;
+            }
+            
+            for(int i=1;i<=10000;i++)
+            {
+                if(numList[i] != 1)
+                    Console.WriteLine(i);
+            }
+        }
+        
+        static int dn(int num)
+        {
+            int rst = num;
+            
+            while(num > 0)
+            {
+                rst+=num%10;
+                num = num/10;
+            }
+            
+            return rst;
+        }
+    }
+}
