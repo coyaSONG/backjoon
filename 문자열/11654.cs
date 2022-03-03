684a32800933a5eb49c9742a5041a81d775a37b0
coyaSONG <66289470+coyaSONG@users.noreply.github.com>
2022-03-03 22:13:07 +0900

    Create 11654.cs

---
+++ 문자열/11654.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace baekjoon
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int answer = (int)Console.ReadLine()[0];
+            Console.WriteLine(answer);
+        }
+    }
+}
