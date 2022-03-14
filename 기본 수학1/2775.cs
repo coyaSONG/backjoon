7f01ed7f52f969d8c2923d0a309651e9ebac4d11
coyaSONG <66289470+coyaSONG@users.noreply.github.com>
2022-03-14 20:48:45 +0900

    Create 2775.cs
    현재 [층 k, 동 n] 은 [k,n-1] 과 [k-1,n]의 합 인 점을 이용함

---
+++ 기본 수학1/2775.cs
@@ -0,0 +1,38 @@
+using System;
+
+namespace baekjoon
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int testNum = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < testNum; i++)
+            {
+                int k = int.Parse(Console.ReadLine());
+                int n = int.Parse(Console.ReadLine());
+                int[,] apart = new int[k+1,n];
+
+
+                for (int j = 0; j < n; j++)
+                {
+                    apart[0,j] = j + 1;
+                }
+
+                for (int c = 0; c <= k; c++)
+                {
+                    for (int d = 0; d < n; d++)
+                    {
+                        if (d == 0)
+                            apart[c, d] = 1;
+                        else if(c != 0)
+                            apart[c, d] = apart[c - 1, d] + apart[c, d - 1];
+                    }
+                }
+
+                Console.WriteLine(apart[k, n-1]);
+            }
+        }
+    }
+}
