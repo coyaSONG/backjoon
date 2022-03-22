7b6d300ab9b6a110fbe922cb39e9d6bdc298071a
coyaSONG <66289470+coyaSONG@users.noreply.github.com>
2022-03-22 19:20:50 +0900

    A+B
    https://www.acmicpc.net/problem/1000

--- 입출력과 사칙연산/1000.cs
+++ 입출력과 사칙연산/1000.cs
@@ -6,7 +6,7 @@ namespace baekjoon
     {
         static void Main(string[] args)
         {
-            string s= Console.ReadLine();
+            string s = Console.ReadLine();
             string[] str = s.Split();
             int a = int.Parse(str[0]);
             int b = int.Parse(str[1]);
