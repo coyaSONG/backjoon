44265cf8dd2f2ebc62b41809be9c190aad91f323
coyaSONG <66289470+coyaSONG@users.noreply.github.com>
2022-04-12 21:13:16 +0900

    N과 M (1)
    https://www.acmicpc.net/problem/15649
    백트래킹 : 값을 깊이 우선 탐색으로 검색 해가 아니면 처음부터 되돌아가서 다시 탐색
    방문한 값을 bool로 관리

---
+++ 백트래킹/15649.cs
@@ -0,0 +1,47 @@
+using System;
+using System.IO;
+using System.Linq;
+
+namespace baekjoon
+{
+    class Program
+    {
+        static StreamReader sr = new StreamReader(new BufferedStream(Console.OpenStandardInput()));
+        static StreamWriter sw = new StreamWriter(new BufferedStream(Console.OpenStandardOutput()));
+        static bool[] visited = new bool[9];
+        static int[] arrNum = new int[9];
+        static int[] input = new int[2];
+        static void Main(string[] args)
+        {
+            input = Array.ConvertAll(sr.ReadLine().Split(), int.Parse);
+
+            DFS(0);
+
+            sw.Flush();
+            sr.Close();
+            sw.Close();
+        }
+
+        static void DFS(int depth)
+        {
+            if (depth == input[1])
+            {
+                sw.WriteLine(string.Join(' ', arrNum.Where(x => x != 0)));
+                return;
+            }
+            else
+            {
+                for (int i = 1; i <= input[0]; i++)
+                {
+                    if (visited[i]) continue;
+
+                    visited[i] = true;
+                    arrNum[depth] = i;
+                    DFS(depth + 1);
+                    visited[i] = false;
+                }
+            }
+
+        }
+    }
+}
