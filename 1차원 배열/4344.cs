c39f837ca04464a6f4be8898a70ee715bcc93730
coyaSONG <66289470+coyaSONG@users.noreply.github.com>
2022-05-02 20:21:02 +0900

    평균은 넘겠지
    https://www.acmicpc.net/problem/4344

--- 1차원 배열/4344.cs
+++ 1차원 배열/4344.cs
@@ -8,7 +8,6 @@ namespace baekjoon
         {
             int n = int.Parse(Console.ReadLine());
 
-
             for (int i = 0; i < n; i++)
             {
                 string[] str = Console.ReadLine().Split();
@@ -18,23 +17,18 @@ namespace baekjoon
                 float sum = 0;
 
                 for (int j = 1; j <= p; j++)
-                {
                     sum += float.Parse(str[j]);
-                }
 
                 float avg = 0;
                 avg = sum / p;
 
                 for (int k = 1; k <= p; k++)
-                {
                     if (avg < float.Parse(str[k])) cnt++;
-                }
 
                 string fmt = string.Format("{0:f3}", Math.Round(cnt / p * 100, 3));
 
                 Console.WriteLine(fmt+"%");
             }
-            
         }
     }
 }
