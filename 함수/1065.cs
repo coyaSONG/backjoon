2342bca1cf9214b40c6f7b75e280c996782cfb8f
coyaSONG <66289470+coyaSONG@users.noreply.github.com>
2022-05-03 19:58:38 +0900

    한수
    https://www.acmicpc.net/problem/1065

--- 함수/1065.cs
+++ 함수/1065.cs
@@ -9,9 +9,7 @@ namespace baekjoon
             int n = int.Parse(Console.ReadLine());
             int answer = 0;
             for (int i = 1; i <= n; i++)
-            {
                 if (isHanSu(i)) answer++;
-            }
 
             Console.WriteLine(answer);
         }
@@ -22,9 +20,7 @@ namespace baekjoon
 
             string str = num.ToString();
             if (num < 100)
-            {
                 result = true;
-            }
             else if (num > 99 && num < 1000)
             {
                 int diff1 = str[1] - str[0];
@@ -32,9 +28,7 @@ namespace baekjoon
                 if (diff1 == diff2) result = true;
             }
             else
-            {
                 result = false;
-            }
 
             return result;
         }
