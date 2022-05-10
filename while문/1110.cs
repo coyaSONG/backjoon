94100cc16257650694423b619d164c4a846dcf1b
coyaSONG <66289470+coyaSONG@users.noreply.github.com>
2022-05-10 19:08:28 +0900

    더하기 사이클
    https://www.acmicpc.net/problem/1110

--- while문/1110.cs
+++ while문/1110.cs
@@ -7,15 +7,12 @@ namespace baekjoon
         static void Main(string[] args)
         {
             int inputNum = int.Parse(Console.ReadLine());
-            int cnt=0;
-            int num=inputNum;
+            int cnt=0, num=inputNum;
             
             while(true)
             {
                 num = (num%10)*10 + ((num/10)+(num%10))%10;
-                
                 cnt++;
-                
                 if(num == inputNum) break;
             }
             Console.WriteLine(cnt);
