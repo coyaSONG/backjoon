cab39ed1912c0d44e8790cff76c1192bfe4b1fdd
coyaSONG <66289470+coyaSONG@users.noreply.github.com>
2022-05-26 19:22:34 +0900

    듣보잡
    https://www.acmicpc.net/problem/1764

---
+++ 집합과 맵/1764.cs
@@ -0,0 +1,40 @@
+using System;
+using System.IO;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+        var sr = new StreamReader(new BufferedStream(Console.OpenStandardInput()));
+        var sw = new StreamWriter(new BufferedStream(Console.OpenStandardOutput()));
+
+        string[] input = sr.ReadLine().Split();
+        int N = int.Parse(input[0]);
+        int M = int.Parse(input[1]);
+        
+        string[] arrN = new string[N];
+        
+        for(int i=0; i<N; i++) arrN[i] = sr.ReadLine();
+        
+        Array.Sort(arrN);
+        var hs = new HashSet<string>(arrN);
+        var rst = new List<string>();
+
+        for(int i=0;i<N;i++)
+        {
+            string s = sr.ReadLine();
+            if(hs.Contains(s))
+            {
+                rst.Add(s);
+            }
+        }
+        rst.Sort();
+        sw.WriteLine(rst.Count);
+        sw.WriteLine(string.Join("\n",rst));
+        
+        sr.Close();
+        sw.Flush();
+        sw.Close();
+    }
+}
