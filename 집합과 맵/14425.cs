17b6e86a68f9bd12f520a72b4859d97926b826b7
coyaSONG <66289470+coyaSONG@users.noreply.github.com>
2022-05-29 14:54:12 +0900

    Update 14425.cs

--- 집합과 맵/14425.cs
+++ 집합과 맵/14425.cs
@@ -8,8 +8,7 @@ namespace baekjoon
         {
             Dictionary<string, string> p = new Dictionary<string, string>();
             string[] input = Console.ReadLine().Split();
-            int N = int.Parse(input[0]);
-            int M = int.Parse(input[1]);
+            int N = int.Parse(input[0]), M = int.Parse(input[1]);
             for (int i = 0; i < N; i++)
                 p.Add(Console.ReadLine(),null);
 
